8b7d048a5b2d60879563ae2f35d8fb7c1c073be9
LiaYia <liayia.tw@gmail.com>
2022-07-10 15:01:37 +0800

    update

---
+++ Python/MongoDB/find_3.py
@@ -0,0 +1,20 @@
+import pymongo
+from pprint import pprint
+
+client = pymongo.MongoClient()
+db = client.opendata
+#docs = db.AQI.find({}, {"County":1 , "SiteName": 1 , "AQI": 1})
+
+# 排序 - 依筆劃(反向)
+#docs = db.AQI.find(
+#    {},
+#    {'SiteName': 1 , 'AQI': 1 , '_id': 0 }
+#).collation({'locale':'zh_Hant'})sort('SiteName', -1)
+
+# 查詢 AQI >= 40 資料 , 並由小到大排序
+docs = db.AQI.find(
+    {'AQI': {'$regex' : '[4-9].|[1-9]..'}},
+    {'County':1 , 'SiteName': 1 , 'AQI': 1 , '_id': 0 }
+).collation({'locale':'zh_Hant'})sort('SiteName')
+
+pprint(list(docs))
