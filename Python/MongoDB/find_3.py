65a1731a074bffc191577f2010da0ba83b172480
LiaYia <liayia.tw@gmail.com>
2022-07-10 15:26:53 +0800

    update

--- Python/MongoDB/find_3.py
+++ Python/MongoDB/find_3.py
@@ -12,9 +12,16 @@ db = client.opendata
 #).collation({'locale':'zh_Hant'})sort('SiteName', -1)
 
 # 查詢 AQI >= 40 資料 , 並由小到大排序
-docs = db.AQI.find(
-    {'AQI': {'$regex' : '[4-9].|[1-9]..'}},
-    {'County':1 , 'SiteName': 1 , 'AQI': 1 , '_id': 0 }
-).collation({'locale':'zh_Hant'})sort('SiteName')
+#docs = db.AQI.find(
+#    {'AQI': {'$regex' : '[4-9].|[1-9]..'}},
+#    {'County':1,'SiteName':1,'AQI':1,'_id':0}
+#).collation({'locale':'zh_Hant'})sort('SiteName')
+
+# 更新一筆 $set , 如果找不到資料就新增
+db.AQWI.updateOne(
+    {name: 'May'},
+    {'$set': {age: 50}}, 
+    upsert=True
+)
 
 pprint(list(docs))
